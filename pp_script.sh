b3c68b0057bd8a9ab76930e46580d2e436c55578
Troy M <SM@google.com>
2021-04-15 01:06:24 -0400

    initial_commit

---
+++ pp_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Executing super complex FMU script now!"
+# shellcheck disable=SC2004
+RESULT=$(($PARAM1 + $PARAM2))
+echo "$RESULT"
+echo "RESULT CALCULATED"
+echo "Exiting script..."
+
